9c542aa8d5f304c15b0bab2329366360f481b363
Fabien Marceau <fabien.marceau@gmail.com>
2023-01-13 15:00:25 +0100

    Création Tables

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return (
+       <header className="head">
+           <h1>Tablomathiques</h1>
+            
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/Router.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+// import Footer from "./Components/Footer/Footer";
+import Home from "./pages/Home/Home";
+// import About from "./pages/About/About";
+// import Logement from "./pages/Logement/Logement";
+// import Erreur from "./pages/Erreur/Erreur";
+
+
+function router() {
+
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                {/* <Route path="/a_propos/" element={<About />}></Route>
+                <Route path="/logements/:id" element={<Logement />}></Route>
+                <Route path="*" element={<Erreur />}></Route> */}
+            </Routes>
+            {/* <Footer /> */}
+        </Router>
+
+    );
+
+}
+
+export default router;
