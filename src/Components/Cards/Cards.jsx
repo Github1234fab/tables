e63d084bbc477b19ed9ae0f9173aa3f5550832b7
Fabien Marceau <fabien.marceau@gmail.com>
2023-01-20 17:42:47 +0100

    Ok Code replacer dans Cards.jsx

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,40 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Cards.css";
+import Calculation from '../Calculation/Calculation';
 
 const Cards = () => {
-  // Chiffre aléatoire......................................................................
-  let arrayNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  let randomNumber = arrayNumber[Math.floor(Math.random() * arrayNumber.length)];
-  console.log(randomNumber);
+    const [state, setState] = useState(null);
+    const getValue = (e) => {
+        setState(e.target.value)
+    }
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const randomArray = array[Math.floor(Math.random() * array.length)];
+    console.log(randomArray);
+    const totalCalculation = randomArray * state;
+    console.log(totalCalculation);
 
-  // Sélectionner un bouton pour obtenir une table et création de calcul. avec un chiffre mystère.....................................................................
-
-  function calculation(e) {
-    let buttonsValues = e.target.value;
-    console.log(buttonsValues);
-    // let totalCalculation = randomNumber * buttonsValues;
-    // console.log(totalCalculation);
-    // let randomCalculation = `? x ${buttonsValues} = ${totalCalculation}`;
-  }
-
-  return (
-    <div className='cardContainer'>
-      <div className="buttons_values" onClick={calculation}>
-        <button class="button_value" value="1" >1</button>
-        <button class="button_value" value="2">2</button>
-        <button class="button_value" value="4">4</button>
-        <button class="button_value" value="3">3</button>
-        <button class="button_value" value="5">5</button>
-        <button class="button_value" value="6">6</button>
-        <button class="button_value" value="7">7</button>
-        <button class="button_value" value="8">8</button>
-        <button class="button_value" value="9">9</button>
-        <button class="button_value" value="10">10</button>
-      </div>
-      {/* <p>tu as choisi la table de {buttonsValues}</p> */}
-    {/* <div className="CalculationMysteriousFigure"> {randomCalculation}</div>  */} 
-    </div>
-  );
+    return (
+        <div>
+            <button onClick={getValue} value="1">1</button>
+            <button onClick={getValue} value="2">2</button>
+            <button onClick={getValue} value="3">3</button>
+            <button onClick={getValue} value="4">4</button>
+            <button onClick={getValue} value="5">5</button>
+            <button onClick={getValue} value="6">6</button>
+            <button onClick={getValue} value="7">7</button>
+            <button onClick={getValue} value="8">8</button>
+            <button onClick={getValue} value="9">9</button>
+            <button onClick={getValue} value="10">10</button>
+            <h1 className='table_choose'>{state}</h1>
+            <Calculation randomArray={randomArray} state={state} totalCalculation={totalCalculation} />
+            <h2 className='blind_calculation'>? X {state} = {totalCalculation}</h2>
+        </div>
+    );
 };
 
   export default Cards;
@@ -44,10 +39,4 @@ const Cards = () => {
 
 
 
-    // //Test entre la réponse et randomNumber..................................
-  // FirstPracticeResponseSend.addEventListener("click", () => {
-  //   if (randomNumber == ResponseFirstPractice.value) {
-  //     responseFirstPractice.innerHTML = "Yeah!! Tu gagnes!";
-  //   } else {
-  //     responseFirstPractice.innerHTML = "Uuuurps! T'es vraiment sûr, là?!!!!!!";
-  //   } 
+
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,35 +1,40 @@
-import React, { useState } from 'react';
-import Calculation from '../../Components/Calculation/Calculation';
-// import axios from 'axios';
+import React from 'react';
+// import Calculation from '../../Components/Calculation/Calculation';
+// // import axios from 'axios';
 import("./Home.css");
 
 
 const Home = () => {
-    const [state, setState] = useState(null);
-    const getValue = (e) => {
-        setState(e.target.value)
-    }
-    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const randomArray = array[Math.floor(Math.random() * array.length)];
-    console.log(randomArray);
-    const totalCalculation = randomArray * state;
-    console.log(totalCalculation);
+  
+//     const [state, setState] = useState(null);
+//     const getValue = (e) => {
+//         setState(e.target.value)
+//     }
+//     const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//     const randomArray = array[Math.floor(Math.random() * array.length)];
+//     console.log(randomArray);
+//     const totalCalculation = randomArray * state;
+//     console.log(totalCalculation);
 
     return (
         <div>
-            <button onClick={getValue} value="1">1</button>
-            <button onClick={getValue} value="2">2</button>
-            <button onClick={getValue} value="3">3</button>
-            <button onClick={getValue} value="4">4</button>
-            <button onClick={getValue} value="5">5</button>
-            <button onClick={getValue} value="6">6</button>
-            <button onClick={getValue} value="7">7</button>
-            <button onClick={getValue} value="8">8</button>
-            <button onClick={getValue} value="9">9</button>
-            <button onClick={getValue} value="10">10</button>
-            <h1>{state}</h1>
-            <Calculation randomArray={randomArray} state={state} totalCalculation={totalCalculation} />
+            <h1>rkfhbarkhvb</h1>
         </div>
+//         <div>
+//             <button onClick={getValue} value="1">1</button>
+//             <button onClick={getValue} value="2">2</button>
+//             <button onClick={getValue} value="3">3</button>
+//             <button onClick={getValue} value="4">4</button>
+//             <button onClick={getValue} value="5">5</button>
+//             <button onClick={getValue} value="6">6</button>
+//             <button onClick={getValue} value="7">7</button>
+//             <button onClick={getValue} value="8">8</button>
+//             <button onClick={getValue} value="9">9</button>
+//             <button onClick={getValue} value="10">10</button>
+//             <h1 className='table_choose'>{state}</h1>
+//             <Calculation randomArray={randomArray} state={state} totalCalculation={totalCalculation} />
+//             <h2 className='blind_calculation'>? X {state} = {totalCalculation}</h2>
+//         </div>
     );
 };
 
